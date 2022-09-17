097f77b008704ace0e55f29f584f0ac74e7a1605
martinfacciuto <martin_facciuto@hotmail.com>
2022-09-17 15:24:28 -0300

    [u\]

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ dotenv.config()
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import graphqlSchema from './graphqlSchema.js'
+import path from 'path'
+import favicon from 'serve-favicon'
+
+const { APP_PORT } = process.env
+const __dirname = path.resolve()
 
 const app = express()
-const { APP_PORT } = process.env
 
-app.use('/', graphqlHTTP({
-	graphiql: true,
-	schema: graphqlSchema,
-}))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 
-app.listen(APP_PORT, () => console.log(`UTA-API listening on port ${APP_PORT}!`))
+app.use(
+	'/',
+	graphqlHTTP({
+		graphiql: true,
+		schema: graphqlSchema,
+	})
+)
+
+app.listen(APP_PORT, () =>
+	console.log(`UTA-API listening on port ${APP_PORT}!`)
+)
