cf747a773dc46cb1f166fdd66f355192f28da939
martinfacciuto <martin_facciuto@hotmail.com>
2022-09-18 15:31:47 -0300

    [u]

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ dotenv.config()
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import graphqlSchema from './graphqlSchema.js'
+import logger from './logger.js'
+import cors from 'cors'
 import path from 'path'
 import favicon from 'serve-favicon'
 
-const { APP_PORT } = process.env
+const { APP_PORT, APP_URL } = process.env
 const __dirname = path.resolve()
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 
@@ -21,6 +26,9 @@ app.use(
 	})
 )
 
-app.listen(APP_PORT, () =>
-	console.log(`UTA-API listening on port ${APP_PORT}!`)
-)
+app.use((request, response) => response.status(404).end())
+
+app.listen(APP_PORT, () => {
+	logger.info(`UTA-API on ${APP_URL}`)
+	console.log(`UTA-API on ${APP_URL}`)
+})
